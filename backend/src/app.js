438bf8d4d8d59a798eea540247163f02a2bcf60e
Manmeet Kaur <149240653+ManmeetKaur-28@users.noreply.github.com>
2025-12-01 23:40:20 +0530

    added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ import { Server } from "socket.io";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
