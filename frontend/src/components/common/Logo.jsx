0cba832f75f8f5e7c66877d6df65d870535905df
Manmeet Kaur <149240653+ManmeetKaur-28@users.noreply.github.com>
2025-12-01 23:07:29 +0530

    image fixes

--- frontend/src/components/common/Logo.jsx
+++ frontend/src/components/common/Logo.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import logoWithoutName from "../../assets/logo_withoutName.png";
 import logoName from "../../assets/LOGO_Name.png";
 function Logo() {
   return (
     <span className="flex mx-auto">
-      <img className="h-11 " src={logoWithoutName} alt="Logo Image" />
-      <img className="h-8 mt-4" src={logoName} alt="MINDLOOM" />
+      <img
+        className="h-11 "
+        src="frontend\src\assets\logo_withoutName.png"
+        alt="Logo Image"
+      />
+      <img
+        className="h-8 mt-4"
+        src="frontend\src\assets\LOGO_Name.png"
+        alt="MINDLOOM"
+      />
     </span>
   );
 }
